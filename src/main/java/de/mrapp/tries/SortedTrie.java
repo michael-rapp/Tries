dcab8460580f1e2867d58b5812ebac5455cb9c30
Michael Rapp <michael.rapp90@googlemail.com>
2017-12-09 15:38:27 +0100

    Added comments.

--- src/main/java/de/mrapp/tries/SortedTrie.java
+++ src/main/java/de/mrapp/tries/SortedTrie.java
@@ -17,11 +17,29 @@ import org.jetbrains.annotations.NotNull;
 
 import java.util.NavigableMap;
 
+/**
+ * Defines the interface of a sorted trie. This is a specialization of the interface {@link Trie}
+ * for implementations that guarantee the trie's keys to be stored in a particular order. When
+ * iterating the trie, its entries, keys and values are guaranteed to be traversed in the correct
+ * order.
+ *
+ * To specify the order of the sequences, which are used as the trie's keys. The used sequence must
+ * either implement the interface {@link java.lang.Comparable} or a {@link java.util.Comparator}
+ * implementation must be provided to the trie.
+ *
+ * @param <SequenceType> The type of the sequences, which are used as the trie's keys
+ * @param <ValueType>    The type of the values, which are stored by trie
+ * @author Michael Rapp
+ * @since 1.0.0
+ */
 public interface SortedTrie<SequenceType extends Sequence, ValueType>
         extends NavigableMap<SequenceType, ValueType>, Trie<SequenceType, ValueType> {
 
+    /**
+     * see {@link Trie#subTree(Sequence)}
+     */
     @NotNull
     @Override
-    SortedTrie<SequenceType, ValueType> subTree(@NotNull SequenceType key);
+    SortedTrie<SequenceType, ValueType> subTree(@NotNull SequenceType sequence);
 
 }
--- src/main/java/de/mrapp/tries/Trie.java
+++ src/main/java/de/mrapp/tries/Trie.java
@@ -19,13 +19,51 @@ import org.jetbrains.annotations.Nullable;
 import java.io.Serializable;
 import java.util.Map;
 
+/**
+ * Defines the interface of a trie (also called prefix tree, digital tree or radix tree). A trie is
+ * a tree data structure that can be used to store an associative array where the keys are sequences
+ * (e.g. sequences of characters or lists of digits).
+ *
+ * Tries allow to efficiently search for sequences (and their values) that share a common prefix. As
+ * nodes with a common prefix share the same predecessors, tries also provide some kind of
+ * compression.
+ *
+ * In a trie values are only associated with the leaf nodes and with some inner nodes. As all
+ * successors of a node share a common prefix of the sequence, which is associated with that node,
+ * the position of a value in the trie specifies the key it is associated with. The root node
+ * corresponds to an empty sequence.
+ *
+ * @param <SequenceType> The type of the sequences, which are used as the trie's keys
+ * @param <ValueType>    The type of the values, which are stored by trie
+ * @author Michael Rapp
+ * @see StringTrie Defines the interface of tries that store strings
+ * @since 1.0.0
+ */
 public interface Trie<SequenceType extends Sequence, ValueType> extends
         Map<SequenceType, ValueType>, Serializable {
 
+    /**
+     * Returns the root node of the trie. The returned object is not modifiable, i.e. an {@link
+     * UnsupportedOperationException} will be thrown when attempting to change the state of the
+     * returned node or one of its successors.
+     *
+     * @return The root node of the trie as an instance of the type {@link Node} or null, if the
+     * trie is empty
+     */
     @Nullable
     Node<SequenceType, ValueType> getRootNode();
 
+    /**
+     * Returns the subtree of the node, which corresponds to a specific sequence. If the given
+     * sequence is not contained by the trie, a {@link java.util.NoSuchElementException} will be
+     * thrown.
+     *
+     * @param sequence The sequence as an instance of the generic type {@link SequenceType}. The
+     *                 sequence may not be null
+     * @return The subtree of the node, which corresponds to the given sequence, as an instance of
+     * the type {@link Trie}. The subtree may not be null
+     */
     @NotNull
-    Trie<SequenceType, ValueType> subTree(@NotNull SequenceType key);
+    Trie<SequenceType, ValueType> subTree(@NotNull SequenceType sequence);
 
 }
