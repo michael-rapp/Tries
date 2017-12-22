ddc48d5edd8cb7604f2063ef200192621ec156f0
Michael Rapp <michael.rapp90@googlemail.com>
2017-12-22 04:08:28 +0100

    Added comments.

--- src/main/java/de/mrapp/tries/HashTrie.java
+++ src/main/java/de/mrapp/tries/HashTrie.java
@@ -22,49 +22,40 @@ import org.jetbrains.annotations.Nullable;
 
 import java.util.NoSuchElementException;
 
+/**
+ * A unsorted trie, which stores the successors of nodes in hash maps. The edges between nodes
+ * always correspond to exactly one element of a sequence. Therefore, even if a subsequence of
+ * length n is not shared between multiple keys, the trie contains n nodes to store that
+ * subsequence. As the used hash maps enable to lookup successors in constant time, this
+ * implementation should be preferred over {@link SortedListTrie}, if the order of keys is
+ * irrelevant.
+ *
+ * @param <SequenceType> The type of the sequences, which are used as the trie's keys
+ * @param <ValueType>    The type of the values, which are stored by trie
+ */
 public class HashTrie<SequenceType extends Sequence, ValueType> extends
         AbstractTrie<SequenceType, ValueType> {
 
+    /**
+     * The constant serial version UID.
+     */
     private static final long serialVersionUID = -2250393346732658811L;
 
+    /**
+     * Creates a new unsorted trie, which stores the successors of nodes in hash maps.
+     *
+     * @param rootNode The root node of the trie as an instance of the type {@link Node} or null, if
+     *                 the trie should be empty
+     */
     private HashTrie(@Nullable final Node<SequenceType, ValueType> rootNode) {
         super(rootNode);
     }
 
+    /**
+     * Creates a new empty, unsorted trie, which stores the successors of nodes in hash maps.
+     */
     public HashTrie() {
         super();
-    }
-
-    @NotNull
-    @Override
-    public HashTrie<SequenceType, ValueType> subTrie(@NotNull final SequenceType key) {
-        Node<SequenceType, ValueType> node = getNode(key);
-
-        if (node != null) {
-            Node<SequenceType, ValueType> newRootNode = createRootNode();
-            Node<SequenceType, ValueType> currentNode = newRootNode;
-            SequenceType suffix = key;
-
-            while (!suffix.isEmpty()) {
-                Pair<Node<SequenceType, ValueType>, SequenceType> pair = addSuccessor(currentNode,
-                        suffix);
-                Node<SequenceType, ValueType> successor = pair.first;
-                suffix = pair.second;
-                currentNode = successor;
-            }
-
-            for (SequenceType sequence : node) {
-                Node<SequenceType, ValueType> successor = node.getSuccessor(sequence);
-
-                if (successor != null) {
-                    currentNode.addSuccessor(sequence, successor.clone());
-                }
-            }
-
-            return new HashTrie<>(newRootNode);
-        }
-
-        throw new NoSuchElementException();
     }
 
     @NotNull
@@ -104,6 +95,38 @@ public class HashTrie<SequenceType extends Sequence, ValueType> extends
     protected final void removeSuccessor(@NotNull final Node<SequenceType, ValueType> node,
                                          @NotNull final SequenceType sequence) {
         node.removeSuccessor(sequence);
+    }
+
+    @NotNull
+    @Override
+    public HashTrie<SequenceType, ValueType> subTrie(@NotNull final SequenceType key) {
+        Node<SequenceType, ValueType> node = getNode(key);
+
+        if (node != null) {
+            Node<SequenceType, ValueType> newRootNode = createRootNode();
+            Node<SequenceType, ValueType> currentNode = newRootNode;
+            SequenceType suffix = key;
+
+            while (!suffix.isEmpty()) {
+                Pair<Node<SequenceType, ValueType>, SequenceType> pair = addSuccessor(currentNode,
+                        suffix);
+                Node<SequenceType, ValueType> successor = pair.first;
+                suffix = pair.second;
+                currentNode = successor;
+            }
+
+            for (SequenceType sequence : node) {
+                Node<SequenceType, ValueType> successor = node.getSuccessor(sequence);
+
+                if (successor != null) {
+                    currentNode.addSuccessor(sequence, successor.clone());
+                }
+            }
+
+            return new HashTrie<>(newRootNode);
+        }
+
+        throw new NoSuchElementException();
     }
 
     @Override
